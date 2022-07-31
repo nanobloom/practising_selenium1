2d57a98294e5d4427fd625c70d813f6cc0496eae
Filip <justarandomguy6543@gmail.com>
2022-07-31 21:20:57 +0200

    Improved GPU method & cleared unnecessary comments

--- morele.py
+++ morele.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 #from selenium.common.exceptions import StaleElementReferenceException
 #from selenium.common.exceptions import NoSuchElementException
-#from selenium.webdriver.remote.webelement import WebElement
 
 class Morele(webdriver.Chrome):
     def __init__(self, time=10, path=r"/opt/chromedriver"):
@@ -18,6 +17,7 @@ class Morele(webdriver.Chrome):
         super().__init__()
         self.maximize_window()
         self.implicitly_wait(self.time)
+
 
     def login(self):
         self.get('https://www.morele.net/login')
@@ -34,25 +34,29 @@ class Morele(webdriver.Chrome):
             button = self.find_element(By.XPATH, '//*[@id="login_form"]/button')
             button.click()
 
+
     def GPU(self):
+        def getting_details():
+            product_list_parent = self.find_element(By.CSS_SELECTOR, 'div[data-controller="product-list"]')
+            product_list = product_list_parent.find_elements(By.CSS_SELECTOR, 'div[data-product-position]')
+            for item in product_list:
+                name = item.get_attribute("data-product-name")
+                name = name.lstrip("Karta graficzna ")
+                name = name.split('(')[0]
+                price = item.get_attribute("data-product-price")
+                price = price.split('.')[0] + " zł"
+                print(name + ' ----- ' + price)
+
         field = self.find_element(By.LINK_TEXT, 'Karty graficzne')
         field.click()
+
         time.sleep(3)
-        
-        #down_arrow = self.find_element(By.CSS_SELECTOR, 'span[data-fcollection-toggle=".f-collection-item-8143-2835"]')
-        #action = ActionChains(self)
-        #action.scroll_to_element(down_arrow)
-        #down_arrow = WebDriverWait(self, 15 ,ignored_exceptions=(NoSuchElementException, StaleElementReferenceException, )).until(
-        # expected_conditions.presence_of_element_located((By.XPATH, '//*[@id="form_12"]/div[1]/div/div[2]/div[6]/div[2]/div[1]/div[1]/div[1]/span/i[1]')))
-        #down_arrow = self.find_element(By.XPATH, '//*[@id="form_12"]/div[1]/div/div[2]/div[6]/div[2]/div[1]/div[1]/div[1]/span')
-        #WebDriverWait(self, 15).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, 'span[data-fcollection-toggle=".f-collection-item-8143-2835"]')))
         down_arrow = self.find_element(By.CSS_SELECTOR, 'span[data-fcollection-toggle=".f-collection-item-8143-2835"]')
         down_arrow.click()
-
+        
         RTX_3060 = self.find_element(By.XPATH, '//*[@id="form_12"]/div[1]/div/div[2]/div[6]/div[2]/div[1]/div[1]/div[2]/div[2]/label')
         RTX_3060.click()
 
-        #WebDriverWait(self, 15).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, 'cat-top-filters-item cat-sorting')))
         time.sleep(3)
         sorting_dropdown_menu = self.find_element(By.XPATH, '//*[@id="category"]/div[2]/div[1]/div[6]/div[1]/div[3]/div/div[1]/button')
         sorting_dropdown_menu.click()
@@ -60,13 +64,21 @@ class Morele(webdriver.Chrome):
         ascending_price = self.find_element(By.CSS_SELECTOR, 'li[data-dropdown-value="price|asc"]')
         ascending_price.click()
 
+        try:
+            getting_details()
+        except:
+            getting_details()
+        
+
     def CPU(self):
         field = self.find_element(By.LINK_TEXT, 'Procesory')
         field.click()
 
+
     def SSD(self):
         field = self.find_element(By.LINK_TEXT, 'Dyski SSD')
         field.click()
+
         
     def filtering(self):
         self.get('https://www.morele.net/')
@@ -82,13 +94,15 @@ class Morele(webdriver.Chrome):
         action = ActionChains(self)
         action.move_to_element(hover_field).perform()
 
+
     def choose_category(self, item='GPU'):
         if item == 'GPU':
             self.GPU()
         if item == 'CPU':
             self.CPU()
         if item == 'SSD':
-            self.SSD()            
+            self.SSD() 
+
 
 if __name__=='__main__':
     with Morele() as bot:
